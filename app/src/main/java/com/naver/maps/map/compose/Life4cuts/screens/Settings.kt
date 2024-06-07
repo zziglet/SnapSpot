c47b28a91bde1105e230dfe1a38ae4b19a434a41
MAVRick9 <cltn222@konkuk.ac.kr>
2024-06-07 16:03:54 +0900

    Settings.kt 수정
    Setting

--- app/src/main/java/com/naver/maps/map/compose/Life4cuts/screens/Settings.kt
+++ app/src/main/java/com/naver/maps/map/compose/Life4cuts/screens/Settings.kt
@@ -1,23 +1,121 @@
-package com.naver.maps.map.compose.Life4cuts.screens
-
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.Icon
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.*
+//import com.example.team04.R
+import com.naver.maps.map.compose.demo.R
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun SettingsScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 60.dp, bottom = 80.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text("SnapSpot", fontSize = 45.sp, fontWeight = FontWeight.Bold)
+        }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            item {
+                SettingsItem(
+                    icon = painterResource(id = R.drawable.baseline_person_24),
+                    text = "사용자 프로필 수정",
+                    onClick = { navController.navigate("profile") }
+                )
+            }
+            item {
+                SettingsItem(
+                    icon = painterResource(id = R.drawable.baseline_lock_24),
+                    text = "공개 설정",
+                    onClick = { navController.navigate("public") }
+                )
+            }
+            item {
+                SettingsItem(
+                    icon = painterResource(id = R.drawable.baseline_star_24),
+                    text = "즐겨찾기 설정",
+                    onClick = { navController.navigate("favorites") }
+                )
+            }
+            item {
+                SettingsItem(
+                    icon = painterResource(id = R.drawable.baseline_settings_24),
+                    text = "계정 설정",
+                    onClick = { navController.navigate("account") }
+                )
+            }
+        }
+    }
+}
 
 @Composable
-fun SettingScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize()
+fun SettingsItem(icon: Painter, text: String, onClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClick() }
+            .padding(vertical = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(text = "settings")
+        Image(
+            painter = icon,
+            contentDescription = null,
+            modifier = Modifier
+                .size(50.dp)
+                .padding(end = 16.dp)
+        )
+        Text(text = text, fontSize = 22.sp)
+
     }
+}
+
+@Composable
+fun ProfileScreen(navController: NavController) {
+    //
+}
+
+
+@Composable
+fun PublicSettingScreen(navController: NavController) {
+    //
+}
+
+
+
+@Composable
+fun FavoritesScreen(navController: NavController) {
+    //
+}
+
+
+@Composable
+fun AccountSettingsScreen(navController: NavController) {
+    //
 }
