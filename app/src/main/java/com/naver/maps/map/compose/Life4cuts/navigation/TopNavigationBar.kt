16542d46c5f139c2ace7f8357834cd1a4365c450
unhee2 <dldnsgml7@naver.com>
2024-06-20 16:23:33 +0900

    Topbar onclick 시 indicator 색상 추가했습니다.

--- app/src/main/java/com/naver/maps/map/compose/Life4cuts/navigation/TopNavigationBar.kt
+++ app/src/main/java/com/naver/maps/map/compose/Life4cuts/navigation/TopNavigationBar.kt
@@ -4,15 +4,7 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.NavigationBarItemColors
-import androidx.compose.material3.Tab
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -31,36 +23,37 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import com.naver.maps.map.compose.demo.Life4cuts.NavBarItems
 import com.naver.maps.map.compose.demo.R
 
-@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopNavigationBar(navController: NavController) {
-    NavigationBar(
-        modifier = Modifier
-            .background(color = Color.White)
-            .height(40.dp),
-        contentColor = colorResource(id = android.R.color.white),
-        containerColor = colorResource(id = android.R.color.white),
-    ) {
-        val backStackEntry by navController.currentBackStackEntryAsState()
-        val currentRoute = backStackEntry?.destination?.route
-        if(!(currentRoute.equals("Login") || currentRoute.equals("Register"))){
-            if(currentRoute.equals("Album") || currentRoute.equals("MyAlbum") || currentRoute.equals
-                    ("Bookmark") || currentRoute.equals("Place")){
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = backStackEntry?.destination?.route
+
+    if (!(currentRoute.equals("Login") || currentRoute.equals("Register"))) {
+        if (currentRoute.equals("Album") || currentRoute.equals("MyAlbum") || currentRoute.equals("Bookmark") || currentRoute.equals("Place")) {
+            NavigationBar(
+                modifier = Modifier
+                    .background(color = Color.White)
+                    .height(40.dp),
+                contentColor = colorResource(id = android.R.color.white),
+                containerColor = colorResource(id = android.R.color.white),
+            ) {
                 NavBarItems.albumtopBarItems.forEach { navItem ->
                     NavigationBarItem(
-                        modifier = Modifier.background(color = Color.White),
                         selected = currentRoute == navItem.route,
                         onClick = {
-                            navController.navigate(navItem.route)
-                            {
+                            navController.navigate(navItem.route) {
                                 popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
                                 }
+                                launchSingleTop = true
+                                restoreState = true
                             }
                         },
-                        icon = {
-                        },
+                        icon = { /* No icon */ },
                         label = {
-                            Text(text = navItem.title,
+                            Text(
+                                text = navItem.title,
                                 style = TextStyle(
                                     fontSize = 14.sp,
                                     lineHeight = 19.6.sp,
@@ -68,53 +61,38 @@ fun TopNavigationBar(navController: NavController) {
                                     fontWeight = FontWeight(600),
                                     color = Color(0xFF000000),
                                     textAlign = TextAlign.Center
-                                ))
+                                )
+                            )
                         },
-                        colors = NavigationBarItemColors(
-                            selectedIconColor = Color.Black,
-                            unselectedIconColor = Color.LightGray,
-                            disabledIconColor = Color.LightGray,
-                            selectedIndicatorColor = Color.White,
-                            disabledTextColor = Color.LightGray,
+                        colors = NavigationBarItemDefaults.colors(
+                            selectedIconColor = Color.Red,
+                            unselectedIconColor = Color.Blue,
+                            selectedTextColor = Color.Black,
                             unselectedTextColor = Color.LightGray,
-                            selectedTextColor = Color.Black
+                            indicatorColor = Color.DarkGray
                         )
                     )
                 }
-            }else{
-                TopAppBar(
-                    title = {
-                        Text(
-                            text = "SnapSpot",
-                            style = TextStyle(
-                                fontSize = 14.sp,
-                                lineHeight = 19.6.sp,
-                                fontFamily = FontFamily(Font(R.font.inter)),
-                                fontWeight = FontWeight(600),
-                                color = Color(0xFF000000),
-                                textAlign = TextAlign.Center,
-                            )
-                        )
-                    },
-                    modifier = Modifier
-                        .background(color = Color.White)
-                        .padding(start = 155.dp),
-                    colors = TopAppBarColors(
-                        containerColor = Color.White,
-                        scrolledContainerColor = Color.White,
-                        navigationIconContentColor = Color.White,
-                        titleContentColor = Color.Black,
-                        actionIconContentColor = Color.Black
-                    )
-                )
             }
-        }else{
+        } else {
             TopAppBar(
                 title = {
-
+                    Text(
+                        text = "SnapSpot",
+                        style = TextStyle(
+                            fontSize = 14.sp,
+                            lineHeight = 19.6.sp,
+                            fontFamily = FontFamily(Font(R.font.inter)),
+                            fontWeight = FontWeight(600),
+                            color = Color(0xFF000000),
+                            textAlign = TextAlign.Center,
+                        )
+                    )
                 },
-                modifier = Modifier.background(color = Color.White),
-                colors = TopAppBarColors(
+                modifier = Modifier
+                    .background(color = Color.White)
+                    .padding(start = 155.dp),
+                colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = Color.White,
                     scrolledContainerColor = Color.White,
                     navigationIconContentColor = Color.White,
@@ -123,6 +101,17 @@ fun TopNavigationBar(navController: NavController) {
                 )
             )
         }
+    } else {
+        TopAppBar(
+            title = { /* Empty title for login/register screen */ },
+            modifier = Modifier.background(color = Color.White),
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = Color.White,
+                scrolledContainerColor = Color.White,
+                navigationIconContentColor = Color.White,
+                titleContentColor = Color.Black,
+                actionIconContentColor = Color.Black
+            )
+        )
     }
 }
-
